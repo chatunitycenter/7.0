af9199a2c61422395095b5b67c072acdb15cd044
GabWT333 <gab333github@gmail.com>
2025-09-14 21:44:54 +0200

    Update OWNER_off&on.js

--- plugins/OWNER_off&on.js
+++ plugins/OWNER_off&on.js
@@ -20,7 +20,7 @@ const handler = async (m, { conn, args, command }) => {
     if (content.includes('/*') && content.includes('*/')) return m.reply(global.t('scriptAlreadyOff', userId, groupId) || 'Script è già spento.')
     const lines = content.split('\n')
     const preservedLine = lines.find(line => line.includes('//Plugin fatto da Gabs & 333 Staff'))
-    const rest = lines.filter(line => !line.includes('Plugin fatto da Gabs & 333 Staff'))
+    const rest = lines.filter(line => !line.includes('//Plugin fatto da Gabs & 333 Staff'))
     const newContent = `${preservedLine || ''}\n/*\n${rest.join('\n')}\n*/`
     fs.writeFileSync(filePath, newContent)
     return m.reply(global.t('scriptTurnedOff', userId, groupId, { file }) || `Script ${file} spento.`)
